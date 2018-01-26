e24995100ec7eacaca3c96f0e0bbbde215d80e09
Anatoliy Antoneko <ya.tolik-a@yandex.ru>
2018-01-26 21:47:39 +0300

    some fix. start work

---
+++ config.php
@@ -0,0 +1,10 @@
+<?
+$name_web_site = "mandarinshow.ru"; //Название сайта
+$admin_email = "ya.tolik-a@yandex.ru"; //Почта админа
+$admin_login = "root"; //Логин админа
+$admin_pass = ""; //Пароль админа
+$connect_ip = 'localhost';
+$connect_user = 'root';
+$connect_pass = '';
+$connect_db = 'phprun';
+?>

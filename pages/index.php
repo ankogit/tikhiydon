609dc563c4c0be8d12221a15cc92c095ec651ef9
Anatoliy Antoneko <ya.tolik-a@yandex.ru>
2018-02-02 22:00:53 +0300

    block css

--- pages/index.php
+++ pages/index.php
@@ -5,7 +5,13 @@
 					<div class="container">
 						<div class="row">
 							<div class="item col-md-4">
-								ITEM
+								<img src="/assets/img/test.jpg" alt="">
+								<a href="">
+									<div class="desc">
+										<h3>Some text</h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis beatae nihil officia impedit quidem labore, sit quam.</p>
+									</div>
+								</a>
 							</div>
 							<div class="item col-md-4">
 								ITEM
